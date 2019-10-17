bc9e683c79beb05c01165801843c4319eb63bc57
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2019-10-17 22:28:32 +0200

    Changed compiler to gcc8 in order to make use of c++17

--- Toolchain-RaspberryPi.cmake
+++ Toolchain-RaspberryPi.cmake
@@ -3,14 +3,15 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 
-SET(CMAKE_C_COMPILER ${CMAKE_CURRENT_LIST_DIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER ${CMAKE_CURRENT_LIST_DIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++)
-SET(CMAKE_FIND_ROOT_PATH ${CMAKE_CURRENT_LIST_DIR}/tools/arm-bcm2708/arm-linux-gnueabihf/arm-linux-gnueabihf/sysroot/)
+SET(CMAKE_C_COMPILER arm-linux-gnueabi-gcc-8)
+SET(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++-8)
+#SET(CMAKE_FIND_ROOT_PATH ${CMAKE_CURRENT_LIST_DIR}/tools/arm-bcm2708/arm-linux-gnueabihf/arm-linux-gnueabihf/sysroot/)
 
 # Use our definitions for compiler tools
-SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # Search for libraries and headers in the target directories only
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+# SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+# SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+SET(CMAKE_BUILD_TYPE Release)
 
-add_definitions(-Wall -std=c++11)
+add_definitions(-Wall -std=c++17)
