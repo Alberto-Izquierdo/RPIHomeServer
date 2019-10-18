ea54541d9987c8228f14560222581f4aaf3647c7
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2019-10-18 18:42:06 +0200

    Changed order of build (desktop is faster than RPI)

--- Toolchain-RaspberryPi.cmake
+++ Toolchain-RaspberryPi.cmake
@@ -2,16 +2,7 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
-
 SET(CMAKE_C_COMPILER arm-linux-gnueabi-gcc-8)
 SET(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++-8)
-#SET(CMAKE_FIND_ROOT_PATH ${CMAKE_CURRENT_LIST_DIR}/tools/arm-bcm2708/arm-linux-gnueabihf/arm-linux-gnueabihf/sysroot/)
-
-# Use our definitions for compiler tools
-# SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# Search for libraries and headers in the target directories only
-# SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-# SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-SET(CMAKE_BUILD_TYPE Release)
 
 add_definitions(-Wall -std=c++17)
